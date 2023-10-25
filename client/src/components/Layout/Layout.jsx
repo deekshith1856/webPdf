2e19106c94c09d430209658e83f07f09735cf5b1
deekshith1856 <nemmanideekshith@gmail.com>
2023-10-25 13:56:04 +0530

    replaced vite svg and added pdf svg

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import Header from "./Header";
-import Footer from "./Footer";
 import { Box } from "@chakra-ui/react";
-
-const LayoutContainer = ({ children }) => {
+import { Helmet } from "react-helmet";
+const LayoutContainer = ({ children, description, keywords, author }) => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="UTF-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+      </Helmet>
       <Header />
       <Box minH={"80vh"}>{children}</Box>
     </div>
   );
 };
-
+LayoutContainer.defaultProps = {
+  description: "MERN app for spliting pdfs and storing pdfs in cloud",
+  keywords: "React, pdf-lib, react-pdf, node, firebase",
+  author: "Deekshith",
+};
 export default LayoutContainer;
