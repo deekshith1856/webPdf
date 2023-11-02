16b1425c5994216fcd720f34c62677e87e0ad82e
deekshith1856 <nemmanideekshith@gmail.com>
2023-11-02 09:50:09 +0530

    Added user input functionality for sideDrawer

--- client/src/components/SideDraw/ExtractPages.jsx
+++ client/src/components/SideDraw/ExtractPages.jsx
@@ -2,15 +2,49 @@ import { Box, Button, Center, Input, Radio, Text } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { formatPageNumbers } from "../../utils/helper";
 
-const ExtractPages = ({ pages, handleSubmit }) => {
+const ExtractPages = ({ pages, handleSubmit, setPages }) => {
   const [formatedPageNumbers, setFormatedPageNumbers] = useState();
-
+  const [modify, setModify] = useState(false);
   // Use useEffect to format the page numbers when 'pages' prop changes
 
   useEffect(() => {
     const data = formatPageNumbers(pages);
     setFormatedPageNumbers(data);
   }, [pages]);
+
+  const handleChange = (e) => {
+    setModify(true);
+    setFormatedPageNumbers(e.target.value);
+  };
+  const handleSelect = () => {
+    const pageNumbersArray = formatedPageNumbers.split(",");
+    console.log(pageNumbersArray);
+    const parsedPageNumbersSet = new Set();
+
+    pageNumbersArray.forEach((pageNumberInput) => {
+      const cleanedPagenumber = pageNumberInput.trim();
+
+      if (cleanedPagenumber.includes("-")) {
+        const [start, end] = cleanedPagenumber.split("-").map(Number);
+
+        if (!isNaN(start) && !isNaN(end) && start <= end) {
+          for (let i = start; i <= end; i++) {
+            parsedPageNumbersSet.add(i);
+          }
+        }
+      } else {
+        const pageNumber = Number(cleanedPagenumber);
+        if (!isNaN(pageNumber)) {
+          parsedPageNumbersSet.add(pageNumber);
+        }
+      }
+      const sortedArray = Array.from(parsedPageNumbersSet).sort(
+        (a, b) => a - b
+      );
+
+      setPages(sortedArray);
+    });
+  };
   return (
     <Box display={"flex"} flexDir={"column"} gap={5}>
       <Center>
@@ -25,8 +59,9 @@ const ExtractPages = ({ pages, handleSubmit }) => {
         <Input
           border={"1px solid black"}
           value={formatedPageNumbers}
-          isReadOnly
+          onChange={handleChange}
         />
+        {modify && <Button onClick={handleSelect}>Select Pages</Button>}
       </Box>
       <Center mt={5} width={{ md: "100%" }}>
         <Button width={{ md: "100%" }} colorScheme="red" onClick={handleSubmit}>
--- client/src/components/SideDraw/SideDrawer.jsx
+++ client/src/components/SideDraw/SideDrawer.jsx
@@ -114,7 +114,7 @@ const SidebarContent = ({
     >
       <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
         <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-          Logo
+          Split-PDF
         </Text>
         <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
       </Flex>
